0dcb6775dc6c032e1d075c874222cf5b091fc485
JuliaSaenko <julia.saenko94@gmail.com>
2018-11-22 13:54:31 +0200

    Добавляет функции и черновой вариант отрисовки

--- js/setup.js
+++ js/setup.js
@@ -1,27 +1,54 @@
 'use strict';
 
 var wizardSetup = document.querySelector('.setup');
+var similarListElement = wizardSetup.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var wizards = [];
+var getRandomIntegerFromInterval = function (min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
 
-for (var i = 0; i <= 4; i++) {
-  wizards[i] = {
-    name: (Math.floor(Math.random() * WIZARD_NAMES.length)) + ' ' + (Math.floor(Math.random() * WIZARD_SURNAMES.length)),
-    coatColor: Math.floor(Math.random() * COAT_COLORS.length),
-    eyesColor: Math.floor(Math.random() * EYES_COLOR.length)
+var getRandomElementFromArray = function (array) {
+  return array[getRandomIntegerFromInterval(0, array.length - 1)];
+};
+
+var createWizard = function () {
+  var wizard = {
+    name: getRandomElementFromArray(WIZARD_NAMES) + ' ' + getRandomElementFromArray(WIZARD_SURNAMES),
+    coatColor: getRandomElementFromArray(COAT_COLORS),
+    eyesColor: getRandomElementFromArray(EYES_COLOR),
   };
+  return wizard;
+};
+
+var getWizards = function (count) {
+  var wizards = [];
+  for (var i = 0; i <= count; i++) {
+    wizards.push(createWizard());
+  }
+  return wizards;
+};
+
+var wizards = getWizards(4);
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
 }
+similarListElement.appendChild(fragment);
 
 wizardSetup.classList.remove('hidden');
-
-// getRandomInArray(Array) - нашла еще такой вариант, но не нашла как он работает и подходит ли.
-// Про задание по желанию(можно переставлять фамилии и имена местами) я не поняла пока как это делать. Можешь навести на мысль?
-// Пока искала решение, нашла вот такое(https://github.com/nefe/You-Dont-Need-jQuery/blob/master/README-ru.md):
-// Заметка: document.querySelector и document.querySelectorAll достаточно МЕДЛЕННЫ, старайтесь использовать getElementById, document.getElementsByClassName или document.getElementsByTagName
-// если хотите улучшить производительность.
-// Что в таком случае все-таки лучше?
+wizardSetup.querySelector('.setup-similar').classList.remove('hidden');
